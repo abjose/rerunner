0f36b0d525f3ae625641e27eeaf956ec0aa33bba
Alex B. Jose <ajose@chromium.org>
2015-08-12 10:22:26 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # rerunner
-Chromium extension for rerunning failed trybots
+Chromium extension for rerunning failed trybots.
+
+Love abusing trybots? Hate selecting each failed bot by hand when you want to run them again? 
+
+This extension adds an extra button, "Select Failures", to the "Choose trybots" pop-up window in Rietveld. When you click "Select Failures", the extension will automatically check the boxes for all trybots that have 1) failed at least once, and 2) never passed. Then just click "Update" as normal.
---
+++ rerunner.js
@@ -0,0 +1,82 @@
+// JS for rerunner extension. Adds a "select failures" button to the trybot
+// popup window, so you don't have to select them by hand.
+
+rerunner_HookTrybotLink();
+
+// Hook into link to open trybot-popup
+function rerunner_HookTrybotLink() {
+  // find the most recent patchset
+  var patch_containers = document.getElementsByClassName("rb-patchContainer");
+  var most_recent_patch = -1;
+  for (var i = 0; i < patch_containers.length; ++i) {
+    var patch_number = parseInt(patch_containers[i].id.replace(/\D/g,''));
+    if (patch_number) {
+      if (patch_number > most_recent_patch) most_recent_patch = patch_number;
+    }
+  }
+  if (most_recent_patch === -1) {
+    console.log("Rerunner unable to find most recent patch.");
+    return;
+  }
+
+  // Get the "Choose trybots" link element.
+  var tryjobchange_link = 
+    document.getElementById("tryjobchange-"+most_recent_patch);
+  if (!tryjobchange_link) {
+    console.log("Rerunner unable to find tryjobchange link.");
+    return;
+  }
+
+  // Add "select failures" button to trybot-popup.
+  var trybot_button_divs = document.getElementsByClassName("trybot-button-div");
+  for (var i = 0; i < trybot_button_divs.length; ++i) {
+    var btn = document.createElement("input");
+    btn.type = "button";
+    btn.value = "Select Failures";
+    btn.onclick = function() { rerunner_SelectFailedBots(most_recent_patch); };
+    trybot_button_divs[i].insertBefore(btn, trybot_button_divs[i].firstChild);
+  }
+}
+
+function rerunner_SelectFailedBots(patch_number) {
+  // Get all failures (only in desired patchset)
+  var patch = document.getElementById("ps-"+patch_number);
+  if (!patch) {
+    console.log("Couldn't find most recent patch.");
+    return;
+  }
+  var failures =
+    patch.getElementsByClassName("build-result build-status-color-failure");
+
+  // Extract trybot names from failures
+  var failure_names = {};
+  for (var i = 0; i < failures.length; ++i) {
+    failure_names[failures[i].innerText.trim()] = true;
+  }
+  failure_names = Object.keys(failure_names);
+
+  // We don't want to select a trybot if it has passed at any point, so grab
+  // successes too.
+  var successes =
+    patch.getElementsByClassName("build-result build-status-color-success");
+  var success_names = {};
+  for (var i = 0; i < successes.length; ++i) {
+    success_names[successes[i].innerText.trim()] = true;
+  }
+
+  // For each failure name, if it hasn't been a success before, check its box
+  var trybot_popup = document.getElementById("trybot-popup");
+  if (!trybot_popup) return;
+  var labels = trybot_popup.getElementsByTagName("label");
+  for (var i = 0; i < failure_names.length; ++i) {
+    if (failure_names[i] in success_names) continue;
+    // Have to manually check each label to see if the name matches...
+    for (var j = 0; j < labels.length; ++j) {
+      if (labels[j].innerText === failure_names[i]) {
+        var checkbox_id = labels[j].attributes["for"].nodeValue;
+        document.getElementById(checkbox_id).checked = true;;
+        break;
+      }
+    }
+  }
+}
